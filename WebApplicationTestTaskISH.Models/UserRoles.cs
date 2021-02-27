33c25fbcc4b8de7b72356405d151c62da04310c0
GIn <sodj94@yandex.ru>
2021-02-27 15:19:22 +0300

    Add models

---
+++ WebApplicationTestTaskISH.Models/UserRoles.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebApplicationTestTaskISH.Models
+{
+    public enum UserRoles
+    {
+        None,
+        Admin,
+        User
+    }
+}
