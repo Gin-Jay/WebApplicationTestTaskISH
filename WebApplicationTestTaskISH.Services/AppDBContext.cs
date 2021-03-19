3e218c53f81055e10b32bbc1acebe7d38d313eaf
GIn <sodj94@yandex.ru>
2021-03-19 21:30:36 +0300

    Add EF connection

---
+++ WebApplicationTestTaskISH.Services/AppDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplicationTestTaskISH.Models;
+
+namespace WebApplicationTestTaskISH.Services
+{
+    public class AppDBContext : DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options)
+        {
+        }
+
+        public DbSet<UserModel> Users { get; set; }
+    }
+}
