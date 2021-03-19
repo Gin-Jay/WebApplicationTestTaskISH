10dd4f94cfd43476422a10b01a706c98280328cd
GIn <sodj94@yandex.ru>
2021-03-20 00:23:33 +0300

    DB migration init and annotation

--- WebApplicationTestTaskISH.Models/UserModel.cs
+++ WebApplicationTestTaskISH.Models/UserModel.cs
@@ -6,9 +6,11 @@ namespace WebApplicationTestTaskISH.Models
     {
         public int Id { get; set; }
         [Required(ErrorMessage = "Поле 'Имя' не может быть пустым.")]
+        [MaxLength(50), MinLength(2)]
         public string Name { get; set; }
         [Required(ErrorMessage = "Поле 'Email' не может быть пустым.")]
         [RegularExpression(@"^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$", ErrorMessage = "Пожалуйста, введите корректный email (формат: example@example.com)")]
+        [MaxLength(50), MinLength(2)]
         public string Email { get; set; }
         public string PhotoPath { get; set; }
         public UserRoles? Role { get; set; }
---
+++ WebApplicationTestTaskISH.Services/Migrations/20210319212212_AnnotationInModelAdded.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApplicationTestTaskISH.Services.Migrations
+{
+    public partial class AnnotationInModelAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Users",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Users",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+        }
+    }
+}
